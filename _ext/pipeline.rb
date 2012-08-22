7afc5fbb798a0c131efd7a80560c6bb108185bb2
Marek Goldmann <marek.goldmann@gmail.com>
2012-08-22 13:00:39 +0200

    Proper links in blog.atom feed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,7 +4,6 @@ require 'bugzilla'
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/blog')
   extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 3)
-  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog.atom', :feed_title => "Blog posts")
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::IntenseDebate.new
 
@@ -16,6 +15,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::TagCloud.new(:posts,
                                                 '/blog/tags/index.html',
                                                 :layout=>'base')
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog.atom', :feed_title => "Blog posts")
   extension TagAtomizer.new(:tags => ['fedora'])
   extension Bugzilla.new(:layout => 'blog')
 
