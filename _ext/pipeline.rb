f7ab2d135563e0e2f743b918abd89a40e6f3ad84
Marek Goldmann <marek.goldmann@gmail.com>
2014-01-23 11:05:50 +0100

    Move to disqus

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -7,7 +7,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/blog')
   extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5)
   extension Awestruct::Extensions::Indexifier.new
-  extension Awestruct::Extensions::IntenseDebate.new
+  extension Awestruct::Extensions::Disqus.new
 
   extension Awestruct::Extensions::Tagger.new(:posts,
                                               '/blog/index',
