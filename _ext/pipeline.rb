fb73413faec72c210a54faeb2fa17528071b9f0d
Marek Goldmann <marek.goldmann@gmail.com>
2014-07-24 16:16:05 +0200

    Make disqus work with https

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,12 +2,13 @@ require 'tag-atomizer'
 require 'bugzilla'
 require 'presentations'
 require 'abstract'
+require 'disqus'
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/blog')
   extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5)
   extension Awestruct::Extensions::Indexifier.new
-  extension Awestruct::Extensions::Disqus.new
+  extension Disqus.new
 
   extension Awestruct::Extensions::Tagger.new(:posts,
                                               '/blog/index',
