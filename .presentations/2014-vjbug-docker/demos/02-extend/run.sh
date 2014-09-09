243fc01110d94f6aa4f5fce37cf8c51e74a2e0ae
Marek Goldmann <marek.goldmann@gmail.com>
2014-09-09 20:31:56 +0200

    vJBUG presentation

---
+++ .presentations/2014-vjbug-docker/demos/02-extend/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run -it --rm -p 8080:8080 -p 9990:9990 wildfly-management
---
+++ .presentations/2014-vjbug-docker/demos/04-logging/more_info.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+clear
+
+echo "Blog post with different logging options discussed:"
+echo
+echo "  https://goldmann.pl/blog/2014/07/18/logging-with-the-wildfly-docker-image/"
+echo
+echo "Dockerfiles:"
+echo
+echo "  https://github.com/goldmann/wildfly-docker-logging/"
+
